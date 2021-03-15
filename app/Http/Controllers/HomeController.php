0f3fcaa03a3ddf83f2a8d1578f2b4d2ad21b3806
Juan Carlos Sanchez <jsanchez@airavirtual.com>
2021-03-15 02:51:45 -0600

    o

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,6 +29,8 @@ class HomeController extends Controller
      */
     public function index()
     {
+
+        return response()->redirectTo('/lista_sucursales/encuestas');
         //return view('admin.home');
     }
     public function list_sucursales($tipo)
